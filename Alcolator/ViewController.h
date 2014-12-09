9e2b1788c1e6bf7b2973e324557706760bab0450
Juline Lamusga <juline@gmail.com>
2014-12-09 10:45:13 -0800

    Initial Commit

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Juline Lamusga on 12/9/14.
+//  Copyright (c) 2014 Juline Lamusga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Alcolator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Alcolator
+//
+//  Created by Juline Lamusga on 12/9/14.
+//  Copyright (c) 2014 Juline Lamusga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
